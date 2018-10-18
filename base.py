f2032ea5b895eda5f12871737f8b075a7d0737a1
ssalin <ssalin@pdx.edu>
2018-10-18 12:14:32 -0700

    Add files via upload
    fooling around with draw functions

---
+++ base.py
@@ -0,0 +1,63 @@
+import numpy as np
+import cv2
+import screeninfo
+import math
+
+winname = "neato"
+screen_id = 0
+screen = screeninfo.get_monitors()[screen_id]
+WIDTH = screen.width 
+HEIGHT = screen.height
+crad = 100 #radius of ball
+x = crad
+
+cap = cv2.VideoCapture(0)
+if (cap.isOpened == False):
+    print("ERROR OPENING CAMERA")
+FWIDTH = int(cap.get(3))
+FHEIGHT = int(cap.get(4))
+
+
+def setwin():
+    cv2.namedWindow(winname, cv2.WINDOW_NORMAL)
+    cv2.moveWindow(winname, screen.x-1,screen.y-1)
+    cv2.setWindowProperty(winname,
+                          cv2.WND_PROP_FULLSCREEN,
+                          cv2.WND_PROP_FULLSCREEN)
+def drawball(x):
+    #read from camera
+    ret, img = cap.read()
+    #draw black image
+    #img = np.zeros((HEIGHT,WIDTH,3), np.uint8)
+    #draw circle
+    cv2.circle(img,(x,(y)),crad,(0,0,255),-1)
+    #update image 
+    setwin()
+    cv2.imshow(winname,img)
+    cv2.waitKey(2)
+
+def xright():
+    global x
+    while ((x+crad) < FWIDTH):
+        drawball(x)
+        x = x + 1 
+
+def xleft():
+    global x
+    while (x-crad) > 0:
+        drawball(x)
+        x = x - 1
+
+
+y = int(math.floor(FHEIGHT/2))#until i add vector movement
+
+#img = np.zeros((HEIGHT,WIDTH,3), np.uint8)
+setwin()
+ret, img = cap.read()
+cv2.imshow(winname,img)
+xright()
+xleft()
+xright()
+xleft()
+cap.release()
+cv2.destroyAllWindows()
